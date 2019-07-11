6528fc17002084c256973048b4577865a10f9801
AntoineChn <gaowangchn@gmail.com>
2019-07-11 19:00:50 +0200

    refactor(example_PRC.R): load previous fit
    `

--- R/example_PPC.R
+++ R/example_PPC.R
@@ -50,93 +50,100 @@ data_frame(expected_value) %>%
   xlim(0, 1)
 
 
+# 
+# # Stan model --------------------------------------------------------------
+# 
+# beta_logit_model <- "
+# data {
+#   int N; // number of observations
+#   int P; // number of explanatory variables
+#   int N2; // number of observations to predict
+#   vector[N] Y; // the proportion paid of expected at some period ahead
+#   matrix[N, P] X; // The explanatory variables (make sure no variables known in the future!)
+#   matrix[N2, P] X_new; // explanatory variables for out-of-sample
+# }
+# transformed data {
+# // we an integer Y and a real Y (Y_adj is only here for flexibility of extension)
+#   vector[N] Y_adj;
+#   int Y_int[N];
+#   
+#   for(i in 1:N) {
+#     if(Y[i] == 1.0) {
+#       Y_adj[i] = 1.0;
+#       Y_int[i] = 1;
+#     } else {
+#       Y_adj[i] = Y[i];
+#       Y_int[i] = 0;
+#     }
+#   }
+# }
+# parameters {
+#   matrix[3, P] beta;
+#   vector[3] mu;
+# }
+# model {
+#   // priors 
+#   to_vector(beta) ~ normal(0, .2);
+#   mu ~ normal(0, 1);
+# 
+#   // likelihood
+#   Y_int ~ bernoulli_logit(mu[1] + X*beta[1]');
+#   
+#   for(i in 1:N) {
+#     if(Y_int[i]==0) {
+#       // We constrain alpha and beta to be positive by taking exp()
+# 
+#       Y_adj[i] ~ beta(exp(mu[2] + X[i]*beta[2]'),exp(mu[3] + X[i]*beta[3]'));
+#     }
+#   }
+# }
+# generated quantities {
+#   vector[N2] Y_predict;
+#   int tmp;
+#   for(i in 1:N2) {
+#     tmp = bernoulli_rng(inv_logit(mu[1] + X_new[i]*beta[1]'));
+#     if(tmp ==1) {
+#       Y_predict[i] = 1.0;
+#     } else {
+#       Y_predict[i] = beta_rng(exp(mu[2] + X_new[i]*beta[2]'), exp(mu[3] + X_new[i]*beta[3]'));
+#     }
+#     if(is_nan(Y_predict[i])) {
+#       Y_predict[i] = 1.1;
+#     }
+#   }
+# }
+# 
+# "
+# 
+# 
+# 
+# # fit ---------------------------------------------------------------------
+# 
+# library(rstan)
+# options(mc.cores = parallel::detectCores() - 1)
+# 
+# beta_logit_estimation <- stan(model_code = beta_logit_model, 
+#                               data = list(N = N,
+#                                           P = 1, 
+#                                           N2 = N,
+#                                           Y = outcome,
+#                                           X = matrix(x, N, 1),
+#                                           X_new = matrix(x, N, 1)),
+#                               iter = 600)
 
-# Stan model --------------------------------------------------------------
-
-beta_logit_model <- "
-data {
-  int N; // number of observations
-  int P; // number of explanatory variables
-  int N2; // number of observations to predict
-  vector[N] Y; // the proportion paid of expected at some period ahead
-  matrix[N, P] X; // The explanatory variables (make sure no variables known in the future!)
-  matrix[N2, P] X_new; // explanatory variables for out-of-sample
-}
-transformed data {
-// we an integer Y and a real Y (Y_adj is only here for flexibility of extension)
-  vector[N] Y_adj;
-  int Y_int[N];
-  
-  for(i in 1:N) {
-    if(Y[i] == 1.0) {
-      Y_adj[i] = 1.0;
-      Y_int[i] = 1;
-    } else {
-      Y_adj[i] = Y[i];
-      Y_int[i] = 0;
-    }
-  }
-}
-parameters {
-  matrix[3, P] beta;
-  vector[3] mu;
-}
-model {
-  // priors 
-  to_vector(beta) ~ normal(0, .2);
-  mu ~ normal(0, 1);
-
-  // likelihood
-  Y_int ~ bernoulli_logit(mu[1] + X*beta[1]');
-  
-  for(i in 1:N) {
-    if(Y_int[i]==0) {
-      // We constrain alpha and beta to be positive by taking exp()
-
-      Y_adj[i] ~ beta(exp(mu[2] + X[i]*beta[2]'),exp(mu[3] + X[i]*beta[3]'));
-    }
-  }
-}
-generated quantities {
-  vector[N2] Y_predict;
-  int tmp;
-  for(i in 1:N2) {
-    tmp = bernoulli_rng(inv_logit(mu[1] + X_new[i]*beta[1]'));
-    if(tmp ==1) {
-      Y_predict[i] = 1.0;
-    } else {
-      Y_predict[i] = beta_rng(exp(mu[2] + X_new[i]*beta[2]'), exp(mu[3] + X_new[i]*beta[3]'));
-    }
-    if(is_nan(Y_predict[i])) {
-      Y_predict[i] = 1.1;
-    }
-  }
-}
-
-"
 
 
-
-# fit ---------------------------------------------------------------------
-
-library(rstan)
-options(mc.cores = parallel::detectCores() - 1)
-
-beta_logit_estimation <- stan(model_code = beta_logit_model, 
-                              data = list(N = N,
-                                          P = 1, 
-                                          N2 = N,
-                                          Y = outcome,
-                                          X = matrix(x, N, 1),
-                                          X_new = matrix(x, N, 1)),
-                              iter = 600)
-
-
+# saveRDS(beta_logit_estimation, 'beta_logit_estimation.rds')
+beta_logit_estimation = readRDS('beta_logit_estimation')
 
 # Post analysis -----------------------------------------------------------
 
-predictions <- extract(beta_logit_estimation,"Y_predict" ) %>% 
+predictions <- rstan::extract(beta_logit_estimation,"Y_predict" ) %>% 
   melt()
+
+predictions$Var2 %>% unique()
+predictions$value
+
 
 predictions %>% 
   ggplot() +
